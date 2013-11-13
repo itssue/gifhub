ee77447265f92a6d572121e6a555a6a4da875140
itssue <its.sue@gmail.com>
2013-11-13 08:12:25 -0500

    adding validation to post form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
+	validates :name, uniqueness: true, length: {
+    minimum: 1,
+    maximum: 20,
+  	}
+  	validates :tag, length: {
+    minimum: 1,
+    maximum: 20,
+  	}
+  	validates :category, length: {
+    minimum: 1,
+    maximum: 20,
+  	}
+  	validates :image, :format => URI::regexp(%w(http https))
 end
